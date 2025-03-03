39436efc97d64127d87a903c941b96957310979e
gargmanya1310 <gargmanya1310@gmail.com>
2025-03-03 12:24:07 +0530

    Work with spring data REST

--- src/main/java/com/demo/springboot/firstrestapi/user/UserDetailsRepository.java
+++ src/main/java/com/demo/springboot/firstrestapi/user/UserDetailsRepository.java
@@ -2,9 +2,12 @@ package com.demo.springboot.firstrestapi.user;
 
 import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@RepositoryRestResource
 public interface UserDetailsRepository extends JpaRepository<UserDetails, Long>
 {
 
